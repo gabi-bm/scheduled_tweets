3d0d7938282e689a72fe65bd18951372ed206299
Gabriel Bartesaghi <g.bartesaghi25@gmail.com>
2022-08-09 17:17:26 -0300

    User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   get "about-us", to: "about#index", as: :about
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  get "sign-up", to: "registrations#new"
+  post "sign-up", to: "registrations#create"
+
+  get "sign-in", to: "sessions#new"
+  post "sign-in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
   root to: "main#index"
 end
